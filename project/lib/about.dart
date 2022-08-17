ab405dce5b83545c7e8d9a7d4ec9f9bea2d1c219
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2022-08-17 10:30:27 -0300

    Crete Hamburguer Menu

---
+++ project/lib/about.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatefulWidget {
+  @override
+  _AboutPageState createState() => new _AboutPageState();
+}
+
+class _AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('about'),
+      ),
+    );
+  }
+}
--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: unnecessary_new
+
 import 'package:flutter/material.dart';
+
+import './about.dart';
 
 void main() {
   runApp(const MyApp());
@@ -52,47 +56,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
+      drawer: new Drawer(
+        child: ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: new Text('Nicolas'), 
+              accountEmail: new Text('email@test.com'),
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new NetworkImage('https://gravatar.com/avatar/0aad39801d4bb384f049d5b70b1f6d78?s=400&d=robohash&r=x'),
+              ),
+              ),
+              new ListTile(
+                title: new Text('About Page'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(context, new MaterialPageRoute(
+                    builder: ((BuildContext context) => new AboutPage())
+                  ));
+                },
+              ),
+              new Divider(
+                color: Colors.black,
+                height: 5.0,
+              ),
+              new ListTile(
+                title: new Text('My Restaurants'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(context, new MaterialPageRoute(
+                    builder: ((BuildContext context) => new AboutPage())
+                  ));
+                },
+              )
+              ],
+        ),
+      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
