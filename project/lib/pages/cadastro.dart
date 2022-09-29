787bcbc94d96d43fa45bb17b6ab1d9890b414532
LeiteGabriel <38228714+LeiteGabriel@users.noreply.github.com>
2022-09-29 18:21:05 -0300

    Tela de Cadastro
    Criação da Tela de cadastro

--- project/lib/pages/cadastro.dart
+++ project/lib/pages/cadastro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 
 class CadastroPage extends StatefulWidget {
   @override
@@ -6,90 +7,273 @@ class CadastroPage extends StatefulWidget {
 }
 
 class _CadastroPageState extends State<CadastroPage> {
+  String? identifier;
+  bool isEmpresa = false;
+  bool isUsuario = false;
+
+  final phoneFormatter = MaskTextInputFormatter(mask: '+## (##) #####-####');
+  final cpfFormatter = MaskTextInputFormatter(mask: '###.###.###-##');
+  final cnpjFormatter = MaskTextInputFormatter(mask: '##.###.###/####-##');
+  final birthdateFormatter = MaskTextInputFormatter(mask: '##/##/####');
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Cadastro'),
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Wrap (
-            runSpacing: 40,
-
-            children: <Widget> [
-              SizedBox(height: 20),
-
-              Title(color: Color.fromARGB(255, 144, 146, 144), child: Text('Login')),
-
-              TextFormField(
+      body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(24),
+              child: Wrap(
+                runSpacing: 40,
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Title(
+                      color: Color.fromARGB(255, 144, 146, 144),
+                      child: Text('Login')),
+                  TextFormField(
                       autofocus: true,
                       keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(
-                        color: Colors.black, 
-                        fontSize: 20 ),
+                      style: const TextStyle(color: Colors.black, fontSize: 20),
                       decoration: const InputDecoration(
                         labelText: 'E-mail',
-                        labelStyle: TextStyle(
-                          color: Colors.black
-                        ),
-                      )
-              ),
-              
-              TextFormField(
+                        labelStyle: TextStyle(color: Colors.black),
+                      )),
+                  TextFormField(
+                      autofocus: true,
+                      keyboardType: TextInputType.name,
+                      style: const TextStyle(color: Colors.black, fontSize: 20),
+                      decoration: const InputDecoration(
+                          labelText: 'Nome Completo',
+                          labelStyle: TextStyle(color: Colors.black))),
+                  TextFormField(
                       autofocus: true,
                       obscureText: true,
-                      keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(
-                        color: Colors.black, 
-                        fontSize: 20 ),
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      keyboardType: TextInputType.visiblePassword,
+                      style: const TextStyle(color: Colors.black, fontSize: 20),
                       decoration: const InputDecoration(
-                        labelText: 'Senha',
-                        labelStyle: TextStyle(color: Colors.black)
-                      )
-              ),
-
-              TextFormField(
-                      autofocus: true,
-                      keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(
-                        color: Colors.black, 
-                        fontSize: 20 ),
-                      decoration: const InputDecoration(
-                        labelText: 'Nome',
-                        labelStyle: TextStyle(color: Colors.black)
-                      )
-              ),
-
-      //         ListTile(
-      //   title: Text(
-      //     'Radio $i',
-      //     style: Theme.of(context).textTheme.subtitle1.copyWith(color: i == 5 ? Colors.black38 : Colors.black),
-      //   ),
-      //   leading: Radio(
-      //     value: 'dwedw',
-      //     groupValue: _value,
-      //     activeColor: Color(0xFF6200EE), 
-      //     onChanged: (value) {  },
-      //   ),
-      // ),
-
-              TextFormField(
-                      autofocus: true,
-                      keyboardType: TextInputType.emailAddress,
-                      style: const TextStyle(
-                        color: Colors.black, 
-                        fontSize: 20 ),
-                      decoration: const InputDecoration(
-                        labelText: 'E-mail',
-                        labelStyle: TextStyle(color: Colors.black)
-                      )
-              ),
-            ],
-          )
-        )
-      ),
+                          labelText: 'Senha',
+                          labelStyle: TextStyle(color: Colors.black))),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: RadioListTile(
+                          contentPadding: EdgeInsets.all(0.0),
+                          title: Text("Empresa"),
+                          value: "empresa",
+                          visualDensity: null,
+                          groupValue: identifier,
+                          onChanged: (value) {
+                            setState(() {
+                              identifier = value.toString();
+                              isEmpresa = true;
+                              isUsuario = false;
+                            });
+                          },
+                        ),
+                      ),
+                      Expanded(
+                        child: RadioListTile(
+                          contentPadding: EdgeInsets.all(0.0),
+                          title: Text("Usuário"),
+                          value: "usuario",
+                          visualDensity: null,
+                          groupValue: identifier,
+                          onChanged: (value) {
+                            setState(() {
+                              identifier = value.toString();
+                              isEmpresa = false;
+                              isUsuario = true;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Visibility(
+                      visible: isEmpresa,
+                      child: Wrap(
+                        runSpacing: 40,
+                        children: <Widget>[
+                          SizedBox(height: 20),
+                          TextFormField(
+                            autofocus: true,
+                            keyboardType: TextInputType.number,
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20),
+                            decoration: const InputDecoration(
+                              labelText: 'CNPJ',
+                              labelStyle: TextStyle(color: Colors.black),
+                            ),
+                            inputFormatters: [cnpjFormatter],
+                          ),
+                          TextFormField(
+                              autofocus: true,
+                              keyboardType: TextInputType.text,
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 20),
+                              decoration: const InputDecoration(
+                                labelText: 'Nome do Estabelecimento',
+                                labelStyle: TextStyle(color: Colors.black),
+                              )),
+                          TextFormField(
+                            autofocus: true,
+                            keyboardType: TextInputType.phone,
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20),
+                            decoration: const InputDecoration(
+                              labelText: 'Telefone de Contato do proprietário',
+                              labelStyle: TextStyle(color: Colors.black),
+                            ),
+                            inputFormatters: [phoneFormatter],
+                          ),
+                          TextFormField(
+                              autofocus: true,
+                              keyboardType: TextInputType.text,
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 20),
+                              decoration: const InputDecoration(
+                                labelText: 'Tipo de Comercio',
+                                labelStyle: TextStyle(color: Colors.black),
+                              )),
+                          TextFormField(
+                              autofocus: true,
+                              keyboardType: TextInputType.streetAddress,
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 20),
+                              decoration: const InputDecoration(
+                                labelText: 'Endereco Restaurante',
+                                labelStyle: TextStyle(color: Colors.black),
+                              )),
+                          SizedBox(
+                              width: double.infinity,
+                              child: TextButton(
+                                onPressed: () {},
+                                style: TextButton.styleFrom(
+                                    primary: Colors.black,
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 36, vertical: 20),
+                                    textStyle: const TextStyle(fontSize: 16),
+                                    backgroundColor: Colors.yellow,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(8))),
+                                child: Text('Cadastrar'),
+                              )),
+                        ],
+                      )),
+                  Visibility(
+                      visible: isUsuario,
+                      child: Wrap(
+                        runSpacing: 40,
+                        children: <Widget>[
+                          SizedBox(height: 20),
+                          TextFormField(
+                            autofocus: true,
+                            keyboardType: TextInputType.number,
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20),
+                            decoration: const InputDecoration(
+                              labelText: 'CPF',
+                              labelStyle: TextStyle(color: Colors.black),
+                            ),
+                            inputFormatters: [cpfFormatter],
+                          ),
+                          TextFormField(
+                            autofocus: true,
+                            keyboardType: TextInputType.phone,
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20),
+                            decoration: const InputDecoration(
+                              labelText: 'Telefone',
+                              labelStyle: TextStyle(color: Colors.black),
+                            ),
+                            inputFormatters: [phoneFormatter],
+                          ),
+                          TextFormField(
+                            autofocus: true,
+                            keyboardType: TextInputType.datetime,
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 20),
+                            decoration: const InputDecoration(
+                              labelText: 'Data de Aniversário',
+                              labelStyle: TextStyle(color: Colors.black),
+                            ),
+                            inputFormatters: [birthdateFormatter],
+                          ),
+                          SizedBox(
+                            width: double.infinity,
+                            child: TextButton(
+                              onPressed: () {},
+                              style: TextButton.styleFrom(
+                                  primary: Colors.black,
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 36, vertical: 20),
+                                  textStyle: const TextStyle(fontSize: 16),
+                                  backgroundColor: Colors.yellow,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8))),
+                              child: Text('Cadastrar'),
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
+              ))),
     );
   }
 }
+
+// class DefineUsuario extends StatefulWidget {
+//   @override
+//   State<DefineUsuario> createState() => _DefineUsuario();
+// }
+
+// class _DefineUsuario extends State<DefineUsuario> {
+//   String? identifier; //no radio button will be selected
+//   //String gender = "male"; //if you want to set default value
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Radio Button in Flutter"),
+//         backgroundColor: Color.fromARGB(255, 117, 115, 115),
+//       ),
+//       body: Container(
+//         padding: EdgeInsets.all(10),
+//         child: Column(
+//           children: [
+//             Text(
+//               "Empresa ou Usuário",
+//               style: TextStyle(fontSize: 20),
+//             ),
+//             Divider(),
+//             RadioListTile(
+//               title: Text("Empresa"),
+//               value: "empresa",
+//               groupValue: identifier,
+//               onChanged: (value) {
+//                 setState(() {
+//                   identifier = value.toString();
+//                 });
+//               },
+//             ),
+//             RadioListTile(
+//               title: Text("Usuãrio"),
+//               value: "usuario",
+//               groupValue: identifier,
+//               onChanged: (value) {
+//                 setState(() {
+//                   identifier = value.toString();
+//                 });
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
